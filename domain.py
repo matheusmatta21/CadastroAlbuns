cb08cc4d25183da4fb0fdaf3def96e0505959dca
Matheus Matta <138629402+matheusmatta21@users.noreply.github.com>
2024-02-08 09:58:07 -0300

    Versão 3

---
+++ domain.py
@@ -0,0 +1,30 @@
+import db
+
+
+def pegarDados(nome, album, artista, lancamento):
+    dados = []
+    dados.append(str(nome))
+    dados.append(int(album))
+    dados.append(str(artista))
+    dados.append(str(lancamento))
+    db.registrarAlbum(dados)
+    print(dados)
+    return dados 
+
+
+def pegarAlbum(album):
+    return db.lerAlbum(album)
+
+
+def buscaPorAno(arquivo):
+    return db.lerAlbum(arquivo)
+
+
+def listarAnos(lista, album):
+    db.lerAlbum(album)
+    for i in range(0, len(album)):
+        if album[i][1] in lista:
+            pass
+        else:
+            lista.append(album[i][1])
+    return lista
